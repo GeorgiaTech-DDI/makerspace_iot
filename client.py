7040a58b44a2e96505d09b5316cd67308e230f4b
Amy Truong <amytruong@gatech.edu>
2022-11-06 09:41:01 -0500

    Set up MQTT connection with AWS IoT and publish test messages

---
+++ client.py
@@ -0,0 +1,102 @@
+from awscrt import mqtt
+from awsiot import mqtt_connection_builder
+import logging
+import os
+import sys
+import time
+import json
+from datetime import datetime
+from dotenv import load_dotenv
+load_dotenv()
+
+
+# Setup logger
+logger = logging.getLogger(__name__)
+formatter = logging.Formatter("[%(name)s][%(levelname)s][%(message)s]")
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatter)
+logger.setLevel(logging.DEBUG)
+logger.addHandler(stream_handler)
+
+
+# Environmental variables from .env file
+AWS_CA_FILE_PATH = os.getenv("AWS_CA_FILE_PATH")
+AWS_CERT_FILE_PATH = os.getenv("AWS_CERT_FILE_PATH")
+AWS_KEY_FILE_PATH = os.getenv("AWS_KEY_FILE_PATH")
+AWS_ENDPOINT = os.getenv("AWS_ENDPOINT")
+AWS_PORT = int(os.getenv("AWS_PORT"))
+AWS_MQTT_TOPIC = os.getenv("AWS_MQTT_TOPIC")
+AWS_CLIENT_ID = os.getenv("AWS_CLIENT_ID")
+
+
+# Callback when connection is accidentally lost.
+def on_connection_interrupted(connection, error, **kwargs):
+    logger.error(f"Connection interrupted, error: {error}")
+
+
+# Callback when an interrupted connection is re-established.
+def on_connection_resumed(connection, return_code, session_present, **kwargs):
+    logger.debug(f"Connection resumed, return_code: {return_code}, session_present: {session_present}")
+
+    if return_code == mqtt.ConnectReturnCode.ACCEPTED and not session_present:
+        logger.error("Session did not persist. Resubscribing to existing topics")
+        resubscribe_future, _ = connection.resubscribe_existing_topics()
+
+        # Cannot synchronously wait for resubscribe result because we're on the connection's event-loop thread,
+        # evaluate result with a callback instead.
+        resubscribe_future.add_done_callback(on_resubscribe_complete)
+
+
+def on_resubscribe_complete(resubscribe_future):
+    resubscribe_results = resubscribe_future.result()
+    logger.info(f"Resubscribe results: {resubscribe_results}")
+
+    for topic, qos in resubscribe_results['topics']:
+        if qos is None:
+            sys.exit(f"Server rejected resubscribe to topic: {topic}")
+
+
+def build_direct_mqtt_connection(on_connection_interrupted, on_connection_resumed):
+        proxy_options = None
+        mqtt_connection = mqtt_connection_builder.mtls_from_path(
+            endpoint=AWS_ENDPOINT,
+            port=AWS_PORT,
+            cert_filepath=AWS_CERT_FILE_PATH,
+            pri_key_filepath=AWS_KEY_FILE_PATH,
+            ca_filepath=AWS_CA_FILE_PATH,
+            on_connection_interrupted=on_connection_interrupted,
+            on_connection_resumed=on_connection_resumed,
+            client_id=AWS_CLIENT_ID,
+            clean_session=False,
+            keep_alive_secs=30,
+            http_proxy_options=proxy_options)
+        return mqtt_connection
+
+
+mqtt_connection = build_direct_mqtt_connection(on_connection_interrupted, on_connection_resumed)
+logger.info("Connecting to {} with client ID '{}'".format(AWS_ENDPOINT, AWS_CLIENT_ID))
+connect_future = mqtt_connection.connect()
+
+# Future.result() waits until a result is available
+connect_future.result()
+logger.info("Connected to AWS endpoint: %s", AWS_ENDPOINT)
+
+
+# Publish message to server desired number of times.
+# This step is skipped if message is blank.
+# This step loops forever if count was set to 0.
+for i in range(10):
+    message = f"timestamp: {datetime.now().isoformat()}"
+    logger.info(f"Publishing message to topic '{AWS_MQTT_TOPIC}': {message}")
+    message_json = json.dumps(message)
+    mqtt_connection.publish(
+        topic=AWS_MQTT_TOPIC,
+        payload=message_json,
+        qos=mqtt.QoS.AT_LEAST_ONCE)
+    time.sleep(1)
+
+# Disconnect
+logger.info("Disconnecting...")
+disconnect_future = mqtt_connection.disconnect()
+disconnect_future.result()
+logger.info("Disconnected!")
