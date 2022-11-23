d01633194abf809d5b51ae5c351a64b604106d43
Gate Tangchartsiri <gate.tang@gmail.com>
2022-11-23 06:57:15 -0500

    esp32_iot file added. esp8266_iot.ino file used for example.

---
+++ Arduino-AWS/esp32_iot/esp32_iot.ino
@@ -0,0 +1,107 @@
+#include "certs.h"
+#include <WiFiClientSecure.h>
+#include <MQTTClient.h>
+#include <ArduinoJson.h>
+#include "WiFi.h"
+
+
+// The MQTT topics that this device should publish/subscribe
+#define AWS_IOT_PUBLISH_TOPIC   "esp8266/pub"
+#define AWS_IOT_SUBSCRIBE_TOPIC "esp8266/sub"
+
+WiFiClientSecure net = WiFiClientSecure();
+MQTTClient client = MQTTClient(256);
+
+//For SCT013 Module:
+#define currentRange 20; //20A Current Rating of SCT013; Change if needed.
+#define v_ref 3.3 //Amount of voltage that the ESP32/8266 can supply.
+#define currentPin 34
+
+float readCurrentVal() {
+  float currentVal = 0;
+  float peakVoltage = 0;
+  float voltage_rms = 0;
+  for (int i = 0; i<5; i++){
+    peakVoltage += analogRead(currentPin);
+    delay(1);
+  }
+  peakVoltage = peakVoltage/5;
+  voltage_rms = peakVoltage * 0.707; //Calculating the RMS voltage. 0.707 = sqrt(2)/2
+
+  voltage_rms = (voltage_rms/1024 * v_ref)/2; //Voltage divided by two as it has been amplified by 2 by the SCT013.
+
+  currentVal = voltage_rms * currentRange;
+
+  return currentVal;
+}
+
+void connectAWS()
+{
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+  Serial.println("Connecting to Wi-Fi");
+
+  while (WiFi.status() != WL_CONNECTED){
+    delay(500);
+    Serial.print(".");
+  }
+
+  // Configure WiFiClientSecure to use the AWS IoT device credentials
+  net.setCACert(AWS_CERT_CA);
+  net.setCertificate(AWS_CERT_CRT);
+  net.setPrivateKey(AWS_CERT_PRIVATE);
+
+  // Connect to the MQTT broker on the AWS endpoint we defined earlier
+  client.begin(AWS_IOT_ENDPOINT, 8883, net);
+
+  // Create a message handler
+  client.onMessage(messageHandler);
+
+  Serial.print("Connecting to AWS IOT");
+
+  while (!client.connect(THINGNAME)) {
+    Serial.print(".");
+    delay(100);
+  }
+
+  if(!client.connected()){
+    Serial.println("AWS IoT Timeout!");
+    return;
+  }
+
+  // Subscribe to a topic
+  client.subscribe(AWS_IOT_SUBSCRIBE_TOPIC);
+
+  Serial.println("AWS IoT Connected!");
+}
+
+void publishMessage()
+{
+  StaticJsonDocument<200> doc;
+  doc["time"] = millis();
+  doc["current"] = readCurrentVal();
+  char jsonBuffer[512];
+  serializeJson(doc, jsonBuffer); // print to client
+
+  client.publish(AWS_IOT_PUBLISH_TOPIC, jsonBuffer);
+}
+
+void messageHandler(String &topic, String &payload) {
+  Serial.println("incoming: " + topic + " - " + payload);
+
+//  StaticJsonDocument<200> doc;
+//  deserializeJson(doc, payload);
+//  const char* message = doc["message"];
+}
+
+void setup() {
+  Serial.begin(9600);
+  connectAWS();
+}
+
+void loop() {
+  publishMessage();
+  client.loop();
+  delay(1000);
+}
