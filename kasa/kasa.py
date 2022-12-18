372451b7a692e0908aa5bc325b81e61666a2c1e1
Gate Tangchartsiri <gate.tang@gmail.com>
2022-12-18 07:44:31 -0500

    kasa.py file added with power readings

--- kasa/kasa.py
+++ kasa/kasa.py
@@ -1,1 +1,36 @@
 #https://blog.devgenius.io/controlling-home-lights-and-appliances-with-python-c411eb019724
+
+import asyncio
+from kasa import SmartPlug
+from kasa import Discover
+from kasa import DeviceType
+
+async def main():
+    devices = await Discover.discover()
+    for addr, dev in devices.items():
+        await dev.update()
+        print(f"{addr} >> {dev}") #Will print out the IP address of the Smart Plug as well as the version.
+
+    plug = SmartPlug("IP Address") #NEED TO FILL IN IP ADDRESS OF SMART PLUG
+
+    #Check if plug is on.
+    await plug.update()
+    print(f"Alias: {plug.alias}")
+    print(f"is_on: {plug.is_on}")
+
+    await plug.turn_on()
+
+    await plug.update()
+    plug.is_on #should return true if plug is turned on.
+
+    print(f"Power: {plug.emeter_realtime.power} W")
+    print(f"Voltage: {plug.emeter_realtime.voltage} V")
+    print(f"Current: {plug.emeter_realtime.current} A")
+    print(f"Power Consumed Today: {plug.emeter_today} kW")
+    print(f"Power Counsumed this Month: {plug.emeter_this_month} kW")
+
+if __name__ == '__main__':
+    asyncio.run(main())
+
+
+
