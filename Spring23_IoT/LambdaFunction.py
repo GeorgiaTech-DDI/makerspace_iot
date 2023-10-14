12bda6ed481a0baffa0437c4c32ed648faf93023
cpriest7 <131073785+cpriest7@users.noreply.github.com>
2023-10-14 15:59:46 -0400

    Update LambdaFunction.py to new function

--- Spring23_IoT/LambdaFunction.py
+++ Spring23_IoT/LambdaFunction.py
@@ -5,8 +5,8 @@ import json
 
 # rds settings
 rds_host  = "smart-makerspaces-vip.cpbwp24lqgha.us-east-1.rds.amazonaws.com"
-user_name = "admin"
-password = "VIP!2023"
+user_name = "generaluser"
+password = "smartmakerspacemember"
 db_name = "machine_metrics"
 
 logger = logging.getLogger()
@@ -29,19 +29,37 @@ def lambda_handler(event, context):
     """
    # message = event
    # data = json.loads(message)
+   
+    assetID = event["assetID"]
+
+    # sql_string = "insert into {assetID} "
     
-    drill_temp = event["drill_temp"]
-    sound_level = event["sound_level"]
-    power_level = event["power_level"]
+    # cases for assetID
+    if assetID == "Sanding_WoodRoom":
+        roll_angle = event["roll_angle"]
+        pitch_angle = event["pitch_angle"]
+        accX = event["accX"]
+        accY = event["accY"]
+        accZ = event["accZ"]
+    elif false:
+        something = 5
+        # something
+    elif false:
+        something = 6
+        # something
+    else:
+        something = 7
+        # something
 
     item_count = 0
-    sql_string = f"insert into drill (drill_temp, sound_level, power_level) values({drill_temp}, {sound_level}, {power_level})"
+    # sql_string = f"insert into drill (drill_temp, sound_level, power_level) values({drill_temp}, {sound_level}, {power_level})"
+    sql_string = f"insert into {assetID} (roll_angle, pitch_angle, accX, accY, accZ) values ({roll_angle}, {pitch_angle}, {accX}, {accY}, {accZ})"
 
     with conn.cursor() as cur:
-        cur.execute("create table if not exists drill ( drill_temp  DECIMAL(12,2) NOT NULL, sound_level DECIMAL(12,2) NOT NULL, power_level DECIMAL(12,2) NOT NULL)")
+        cur.execute("create table if not exists Sanding_WoodRoom (roll_angle  DECIMAL(12,2), pitch_angle DECIMAL(12,2), accX DECIMAL(12,2),  accY DECIMAL(12,2),  accZ DECIMAL(12,2), recorded_at datetime not null default current_timestamp);")
         cur.execute(sql_string)
         conn.commit()
-        cur.execute("select * from drill")
+        cur.execute("select * from Sanding_WoodRoom")
         logger.info("The following items have been added to the database:")
         for row in cur:
             item_count += 1
